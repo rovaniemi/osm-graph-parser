dedf5be19630b06d42cb1cbebf1fe06f616c1f7f
Rovaniemi <mauri.karlin@helsinki.fi>
2017-06-26 10:34:06 +0300

    remove weight dropper and added test for idNormalizer

--- src/main/java/osmparser/IdNormalizer.java
+++ src/main/java/osmparser/IdNormalizer.java
@@ -10,13 +10,10 @@ public class IdNormalizer {
 
     public List<Node> normalizeIds(Map<Long, Node> graph) {
         normalize(graph);
-        int size = newGraph.size();
-        weightDropper();
-        if(size != newGraph.size()) normalize(newGraph);
         return new ArrayList<>(newGraph.values());
     }
 
-    public void normalize(Map<Long, Node> graph){
+    private void normalize(Map<Long, Node> graph){
         reset();
         oldGraph = graph;
         createNodesWithNewIds();
@@ -51,25 +48,6 @@ public class IdNormalizer {
         for (Weight weight : weights) {
             Long newTargetId = oldIdToNewId.get(weight.getI());
             newNode.addEdgeTo(newTargetId, weight.getW());
-        }
-    }
-
-    // Drop weight if the weight i is bigger than map size.
-    // Actually I don´t know is there any possible cases where this can happen.
-    private void weightDropper(){
-        Iterator i = newGraph.keySet().iterator();
-        int size = newGraph.size();
-        while(i.hasNext()){
-            long l = (long) i.next();
-            Set<Weight> weights = newGraph.get(l).getE();
-            Iterator j = weights.iterator();
-            while(j.hasNext()){
-                Weight e = (Weight) j.next();
-                if(e.getI() > size){
-                    j.remove();
-                }
-            }
-            if (weights.isEmpty()) i.remove();
         }
     }
 
---
+++ src/test/java/omsparser/IdNormalizerTest.java
@@ -0,0 +1,54 @@
+package omsparser;
+
+import org.junit.*;
+import osmparser.IdNormalizer;
+import osmparser.Node;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IdNormalizerTest {
+
+    public IdNormalizerTest (){
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void normalizerWorks(){
+        Map<Long, Node> oldMap = new HashMap<Long, Node>();
+        IdNormalizer idNormalizer = new IdNormalizer();
+        for (int i = 0; i < 10000; i++) {
+            oldMap.put(i * 2l, new Node(i * 2, 16.22, 12.60));
+        }
+        for (Long l:oldMap.keySet()) {
+            oldMap.get(l).addEdgeTo(2,(int) (2 * l));
+        }
+        List<Node> newMap = idNormalizer.normalizeIds(oldMap);
+        for (int i = 0; i < newMap.size(); i++) {
+            assertTrue(newMap.get(i).getId() == i);
+            assertTrue(newMap.get(i).getE().size() == 1);
+        }
+
+    }
+}
